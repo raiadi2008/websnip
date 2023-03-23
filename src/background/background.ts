81a12e1fac57957e79660336a8fef1134912cfbb
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-03-23 22:56:22 +0530

    disconnected event setup in background.ts

--- src/background/background.ts
+++ src/background/background.ts
@@ -2,4 +2,7 @@ chrome.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener((message) => {
     console.log(message)
   })
+  port.onDisconnect.addListener((port) => {
+    console.log("stupid tab disconnected")
+  })
 })
