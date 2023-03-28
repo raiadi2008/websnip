27e5fbc76279f100408728af8a871c2e6cbbed68
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-03-28 17:10:01 +0530

    fixed changes

--- src/html_parser/html_parser.ts
+++ src/html_parser/html_parser.ts
@@ -2,6 +2,7 @@ import { VOID_ELEMENTS } from "../constants/constants"
 import { HtmlClassInfoInterface } from "../types/types"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const classMap = new Map<string, string>()
 
 let classNumber = 0
@@ -27,39 +28,25 @@ export const getAncestorHtml = (target: HTMLElement): string => {
 >>>>>>> 49f9071 (changes)
   const stack: HTMLElement[] = []
   let currentElement: HTMLElement | null = target
+=======
+const classMap = new Map<string, string>()
+>>>>>>> f58068a (fixed changes)
 
-  while (currentElement) {
-    stack.push(currentElement)
-    currentElement = currentElement.parentElement
+let classNumber = 0
+
+const addNewClassNamesToHtml = (target: Element) => {
+  classNumber += 1
+  const newClassName = `websnip-genrated-class-${classNumber}`
+  target.classList.add(newClassName)
+  classMap.set(target.className, newClassName)
+  for (const childElement of Array.from(target.children)) {
+    addNewClassNamesToHtml(childElement)
   }
+}
 
-  let result = ""
-  const closingTags: string[] = []
-
-  while (stack.length > 0) {
-    const element = stack.pop()!
-    const tagName = element.tagName.toLowerCase()
-    const isVoidElement = VOID_ELEMENTS.includes(tagName)
-
-    let openingTag = `<${tagName}`
-    for (const attr of element.attributes) {
-      openingTag += ` ${attr.name}="${attr.value}"`
-    }
-    openingTag += isVoidElement ? "/>" : ">"
-    result += openingTag
-
-    if (element === target && !isVoidElement) {
-      result += element.innerHTML
-    }
-
-    if (!isVoidElement) {
-      closingTags.push(`</${tagName}>`)
-    }
-  }
-
-  while (closingTags.length > 0) {
-    result += closingTags.pop()!
-  }
-
-  return result
+export const getAncestorHtml = (target: HTMLElement): Map<string, string> => {
+  classMap.clear()
+  classNumber = 0
+  addNewClassNamesToHtml(target)
+  return classMap
 }
