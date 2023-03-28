ff5cf4ed15b50938e9bc1901567b2e05f994ec08
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-03-28 17:10:01 +0530

    fixed changes

--- src/html_parser/html_parser.ts
+++ src/html_parser/html_parser.ts
@@ -5,6 +5,7 @@ import { HtmlClassInfoInterface } from "../types/types"
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const classMap = new Map<string, string>()
 
 let classNumber = 0
@@ -28,6 +29,8 @@ export const getAncestorHtml = (target: HTMLElement): Map<string, string> => {
 >>>>>>> c3f43f8 (arrow function changes)
 export function getAncestorHtml(target: HTMLElement): string {
 =======
+=======
+>>>>>>> 9d7de45 (fixed changes)
 export const getAncestorHtml = (target: HTMLElement): string => {
 >>>>>>> 49f9071 (changes)
 =======
@@ -49,6 +52,52 @@ const addNewClassNamesToHtml = (target: Element) => {
   for (const childElement of Array.from(target.children)) {
     addNewClassNamesToHtml(childElement)
   }
+<<<<<<< HEAD
+=======
+
+  let result = ""
+  const closingTags: string[] = []
+
+  while (stack.length > 0) {
+    const element = stack.pop()!
+    const tagName = element.tagName.toLowerCase()
+    const isVoidElement = VOID_ELEMENTS.includes(tagName)
+
+    let openingTag = `<${tagName}`
+    for (const attr of element.attributes) {
+      openingTag += ` ${attr.name}="${attr.value}"`
+    }
+    openingTag += isVoidElement ? "/>" : ">"
+    result += openingTag
+
+    if (element === target && !isVoidElement) {
+      result += element.innerHTML
+    }
+
+    if (!isVoidElement) {
+      closingTags.push(`</${tagName}>`)
+    }
+  }
+
+  while (closingTags.length > 0) {
+    result += closingTags.pop()!
+  }
+
+  return result
+=======
+const classMap = new Map<string, string>()
+
+let classNumber = 0
+
+const addNewClassNamesToHtml = (target: Element) => {
+  classNumber += 1
+  const newClassName = `websnip-genrated-class-${classNumber}`
+  target.classList.add(newClassName)
+  classMap.set(target.className, newClassName)
+  for (const childElement of Array.from(target.children)) {
+    addNewClassNamesToHtml(childElement)
+  }
+>>>>>>> 9d7de45 (fixed changes)
 }
 
 export const getAncestorHtml = (target: HTMLElement): Map<string, string> => {
@@ -56,4 +105,8 @@ export const getAncestorHtml = (target: HTMLElement): Map<string, string> => {
   classNumber = 0
   addNewClassNamesToHtml(target)
   return classMap
+<<<<<<< HEAD
+=======
+>>>>>>> 1a48af3 (fixed changes)
+>>>>>>> 9d7de45 (fixed changes)
 }
