3becc240014bb19280dfa8cdc19630388f148bad
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-03-28 18:07:31 +0530

    added css parser

--- src/css_parser/css_parser.ts
+++ src/css_parser/css_parser.ts
@@ -1,9 +1,58 @@
-import { getAncestorSelector } from "../html_parser/html_parser"
+import { pseudoClassRegex } from "../constants/constants"
 
-export function collectCssRules(
+const getCssTextWithoutSelectors = (cssText: string) => {
+  const openingBraceIndex = cssText.indexOf("{")
+  const closingBraceIndex = cssText.lastIndexOf("}")
+  let cssProps: string = null
+  if (
+    openingBraceIndex >= 0 &&
+    closingBraceIndex >= 0 &&
+    closingBraceIndex > openingBraceIndex
+  ) {
+    cssProps = cssText
+      .substring(openingBraceIndex, closingBraceIndex + 1)
+      .trim()
+  }
+
+  return cssProps
+}
+
+const getAncestorSelector = (element: HTMLElement | null): string => {
+  const segments: string[] = []
+  while (element && element.nodeType === Node.ELEMENT_NODE) {
+    let segment = element.tagName.toLowerCase()
+    if (element.id) {
+      segment += "#" + element.id
+    }
+    if (element.className) {
+      segment += "." + element.className.trim().replace(/\s+/g, ".")
+    }
+    segments.unshift(segment)
+    element = element.parentElement
+  }
+  return segments.join(" > ")
+}
+
+const isPseudoClass = (element: HTMLElement, selectorText: string): string => {
+  const selectorsArray: string[] = selectorText.split(",")
+  const pseudoSelectors = new Set<string>()
+  for (const selector of selectorsArray) {
+    if (element.matches(selector)) {
+      const match = selectorText.match(pseudoClassRegex)
+      match.forEach((value) => pseudoSelectors.add(value))
+    }
+  }
+  if (pseudoSelectors.size > 0) {
+    return Array.from(pseudoSelectors).join(":")
+  }
+  return null
+}
+
+export const collectCssRules = (
   element: HTMLElement,
+  classMap: Map<string, string>,
   cssRules: string[] = []
-): string[] {
+): string[] => {
   // Collect CSS rules from stylesheets
   for (const stylesheet of Array.from(document.styleSheets)) {
     try {
@@ -12,7 +61,15 @@ export function collectCssRules(
           rule instanceof CSSStyleRule &&
           element.matches(rule.selectorText)
         ) {
-          cssRules.push(rule.cssText)
+          console.log(rule)
+          const appliedPseudo = isPseudoClass(element, rule.selectorText)
+
+          const cssProps = getCssTextWithoutSelectors(rule.cssText)
+          let selector = `.${classMap[element.className]}`
+          if (appliedPseudo) {
+            selector = `${selector}:${appliedPseudo}`
+          }
+          cssRules.push(`${selector}${cssProps}`)
         }
       }
     } catch (error) {
@@ -29,8 +86,7 @@ export function collectCssRules(
 
   // Recursively collect CSS rules for child elements
   for (const child of Array.from(element.children)) {
-    collectCssRules(child as HTMLElement, cssRules)
+    collectCssRules(child as HTMLElement, classMap, cssRules)
   }
-
   return cssRules
 }
